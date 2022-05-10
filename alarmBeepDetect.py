56c802284ff54cc40240cf461b89b578ceb34eb3
Ramon Arguello <ramonarguello@monzo.com>
2022-05-10 13:47:16 +0200

    Customise frequency, add mic config, and crontab backup

--- alarmBeepDetect.py
+++ alarmBeepDetect.py
@@ -3,6 +3,8 @@
 # Benjamin Chodroff benjamin.chodroff@gmail.com
 
 import pyaudio
+import requests
+from requests.exceptions import HTTPError
 from numpy import zeros,linspace,short,fromstring,hstack,transpose,log
 from scipy import fft
 from time import sleep
@@ -10,15 +12,15 @@ from time import sleep
 #Volume Sensitivity, 0.05: Extremely Sensitive, may give false alarms
 #             0.1: Probably Ideal volume
 #             1: Poorly sensitive, will only go off for relatively loud
-SENSITIVITY= 1
+SENSITIVITY= 0.1
 # Alarm frequency (Hz) to detect (Set frequencyoutput to True if you need to detect what frequency to use)
-TONE = 3500
+TONE = 1529
 #Bandwidth for detection (i.e., detect frequencies +- within this margin of error of the TONE)
 BANDWIDTH = 30
 #How many 46ms blips before we declare a beep? (Set frequencyoutput to True if you need to determine how many blips are found, then subtract some)
-beeplength=8
+beeplength=2
 # How many beeps before we declare an alarm? (Avoids false alarms)
-alarmlength=5
+alarmlength=3
 # How many false 46ms blips before we declare there are no more beeps? (May need to be increased if there are expected long pauses between beeps) 
 resetlength=10
 # How many reset counts until we clear an active alarm? (Keep the alarm active even if we don't hear this many beeps)
@@ -31,7 +33,7 @@ frequencyoutput=False
 
 # Audio Sampler
 NUM_SAMPLES = 2048
-SAMPLING_RATE = 44100
+SAMPLING_RATE = 22050
 pa = pyaudio.PyAudio()
 _stream = pa.open(format=pyaudio.paInt16,
                   channels=1, rate=SAMPLING_RATE,
@@ -66,7 +68,7 @@ while True:
             thefreq = which*SAMPLING_RATE/NUM_SAMPLES
     if max(intensity[(frequencies < TONE+BANDWIDTH) & (frequencies > TONE-BANDWIDTH )]) > max(intensity[(frequencies < TONE-1000) & (frequencies > TONE-2000)]) + SENSITIVITY:
         if frequencyoutput:
-            print "\t\t\t\tfreq=",thefreq
+            print "\t\t\t\tfreq IN=",thefreq
         blipcount+=1
         resetcount=0
         if debug: print "\t\tBlip",blipcount
@@ -80,9 +82,24 @@ while True:
                 alarm=True
                 print "Alarm!"
                 beepcount=0
+		_stream.stop_stream()
+		_stream.close()	
+		try:
+		    response = requests.get('https://api.telegram.org/{YOUR_BOT_TOKEN}/sendMessage?text=Puerta!&chat_id={YOUR_CHAT_ID}')
+                    response.raise_for_status()
+                except HTTPError as http_err:
+                    print "HTTP error occurred", http_err
+                except Exception as err:
+                    print "Other error occurred:", err
+                else:
+		    sleep(60)
+		_stream = pa.open(format=pyaudio.paInt16,
+    			          channels=1, rate=SAMPLING_RATE,
+                  		  input=True,
+                  		  frames_per_buffer=NUM_SAMPLES)
     else:
         if frequencyoutput:
-            print "\t\t\t\tfreq="
+            print "\t\t\t\tfreq OUT=",thefreq
         blipcount=0
         resetcount+=1
         if debug: print "\t\t\treset",resetcount
